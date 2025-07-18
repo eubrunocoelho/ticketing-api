69c455242ffdee8e2f295f99567658f1ee61088c
eubrunocoelho <eu.brunocoelho94@gmail.com>
2025-07-18 12:28:38 -0300

    feat: adds automatic value for timestamps

--- src/main/java/com/eubrunocoelho/ticketing/entity/Users.java
+++ src/main/java/com/eubrunocoelho/ticketing/entity/Users.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Setter
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class Users {
 
     public static enum Role {
