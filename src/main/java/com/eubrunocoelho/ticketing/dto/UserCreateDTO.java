5424444036e144e354347da5763f8a45eb832b4b
eubrunocoelho <eu.brunocoelho94@gmail.com>
2025-07-18 13:03:44 -0300

    feat: add regex to validate username

--- src/main/java/com/eubrunocoelho/ticketing/dto/UserCreateDTO.java
+++ src/main/java/com/eubrunocoelho/ticketing/dto/UserCreateDTO.java
@@ -4,6 +4,7 @@ import com.eubrunocoelho.ticketing.annotation.validation.UniqueEmail;
 import com.eubrunocoelho.ticketing.annotation.validation.UniqueUsername;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateDTO(
@@ -17,6 +18,10 @@ public record UserCreateDTO(
         @NotBlank(message = "O valor para \"username\" é obrigatório.")
         @Size(min = 8, message = "O valor para \"username\" deve ter no mínimo 8 caracteres.")
         @Size(max = 32, message = "O valor para \"username\" deve ter no máximo 32 caracteres.")
+        @Pattern(
+                regexp = "^(?!\\\\d)[a-zA-Z_][a-zA-Z0-9_]*$",
+                message = "O valor para \"username\" deve começar com letra ou _ (underscore), conter apenas letras, números e _ (underscore), e não pode iniciar com número."
+        )
         @UniqueUsername
         String username,
 
