1c4d18c3a73936a6428419a789cb38abe4d5f6b1
eubrunocoelho <eu.brunocoelho94@gmail.com>
2025-10-30 12:49:38 -0300

    style: modify code style

--- src/main/java/com/eubrunocoelho/ticketing/dto/user/UserCreateDto.java
+++ src/main/java/com/eubrunocoelho/ticketing/dto/user/UserCreateDto.java
@@ -8,16 +8,15 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateDto(
-
-        @NotBlank(message = "O valor para \"email\" é obrigatório.")
-        @Email(message = "O valor para \"email\" deve ser um endereço de e-mail válido.")
-        @Size(max = 128, message = "O valor para \"email\" está muito longo.")
+        @NotBlank( message = "O valor para \"email\" é obrigatório." )
+        @Email( message = "O valor para \"email\" deve ser um endereço de e-mail válido." )
+        @Size( max = 128, message = "O valor para \"email\" está muito longo." )
         @UniqueEmail
         String email,
 
-        @NotBlank(message = "O valor para \"username\" é obrigatório.")
-        @Size(min = 8, message = "O valor para \"username\" deve ter no mínimo 8 caracteres.")
-        @Size(max = 32, message = "O valor para \"username\" deve ter no máximo 32 caracteres.")
+        @NotBlank( message = "O valor para \"username\" é obrigatório." )
+        @Size( min = 8, message = "O valor para \"username\" deve ter no mínimo 8 caracteres." )
+        @Size( max = 32, message = "O valor para \"username\" deve ter no máximo 32 caracteres." )
         @Pattern(
                 regexp = "^(?!\\\\d)[a-zA-Z_][a-zA-Z0-9_]*$",
                 message = "O valor para \"username\" deve começar com letra ou _ (underscore), conter apenas letras, números e _ (underscore), e não pode iniciar com número."
@@ -25,9 +24,10 @@ public record UserCreateDto(
         @UniqueUsername
         String username,
 
-        @NotBlank(message = "O valor para \"password\" é obrigatório.")
-        @Size(min = 8, message = "O valor para \"password\" deve ter no mínimo 8 caracteres.")
-        @Size(max = 32, message = "O valor para \"password\" deve ter no máximo 32 caracteres.")
+        @NotBlank( message = "O valor para \"password\" é obrigatório." )
+        @Size( min = 8, message = "O valor para \"password\" deve ter no mínimo 8 caracteres." )
+        @Size( max = 32, message = "O valor para \"password\" deve ter no máximo 32 caracteres." )
         String password
-) {
+)
+{
 }
