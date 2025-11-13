9010b498e7a01a20e1330234cfce1cdc0bb36c1b
eubrunocoelho <eu.brunocoelho94@gmail.com>
2025-11-13 16:22:10 -0300

    refactor: refactor jackson config class

--- src/main/java/com/eubrunocoelho/ticketing/config/JacksonConfig.java
+++ src/main/java/com/eubrunocoelho/ticketing/config/JacksonConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
@@ -21,27 +22,49 @@ public class JacksonConfig
     @Bean
     public ObjectMapper objectMapper( Jackson2ObjectMapperBuilder builder )
     {
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(
-                java.time.LocalDateTime.class,
-                new LocalDateTimeSerializer( DATE_TIME_FORMATTER )
-        );
-
         return builder
-                .modules( javaTimeModule, hibernate6Module() )
-                .featuresToDisable( SerializationFeature.WRITE_DATES_AS_TIMESTAMPS )
-                .serializationInclusion( JsonInclude.Include.NON_NULL )
+                .modules(
+                        javaTimeModule(),
+                        hibernate6Module()
+                )
+                .featuresToDisable(
+                        SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
+                )
+                .serializationInclusion(
+                        JsonInclude.Include.NON_NULL
+                )
                 .build();
+    }
+
+    @Bean
+    public JavaTimeModule javaTimeModule()
+    {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+        javaTimeModule
+                .addSerializer(
+                        LocalDateTime.class,
+                        new LocalDateTimeSerializer(
+                                DATE_TIME_FORMATTER
+                        )
+                );
+
+        return javaTimeModule;
     }
 
     @Bean
     public Hibernate6Module hibernate6Module()
     {
-        Hibernate6Module module = new Hibernate6Module();
+        Hibernate6Module hibernate6Module = new Hibernate6Module();
 
-        module.disable( Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION );
-        module.enable( Hibernate6Module.Feature.FORCE_LAZY_LOADING );
+        hibernate6Module.disable(
+                Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION
+        );
 
-        return module;
+        hibernate6Module.enable(
+                Hibernate6Module.Feature.FORCE_LAZY_LOADING
+        );
+
+        return hibernate6Module;
     }
 }
