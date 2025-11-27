d58e161313545cd7c23c12e58db83846431373f7
eubrunocoelho <eu.brunocoelho94@gmail.com>
2025-11-27 16:33:15 -0300

    build: add dotenv library

---
+++ src/main/java/com/eubrunocoelho/ticketing/config/DotenvEnvironmentConfig.java
@@ -0,0 +1,42 @@
+package com.eubrunocoelho.ticketing.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+
+import java.util.Properties;
+
+public class DotenvEnvironmentConfig implements EnvironmentPostProcessor
+{
+    @Override
+    public void postProcessEnvironment(
+            ConfigurableEnvironment environment,
+            SpringApplication application
+    )
+    {
+        Dotenv dotenv = Dotenv
+                .configure()
+                .ignoreIfMissing()
+                .load();
+
+        Properties props = new Properties();
+
+        dotenv
+                .entries()
+                .forEach( entry -> props.put(
+                        entry.getKey(),
+                        entry.getValue()
+                ) );
+
+        environment
+                .getPropertySources()
+                .addFirst(
+                        new PropertiesPropertySource(
+                                "dotenvProperties",
+                                props
+                        )
+                );
+    }
+}
